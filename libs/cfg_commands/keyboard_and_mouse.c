8651d9f4d0bb2151159ecced9ab863f7468d07e1
Agus <valenzuela.agustin.tomas@gmail.com>
2025-05-20 14:57:30 -0300

    keyboard_and_mouse

---
+++ libs/cfg_commands/keyboard_and_mouse.c
@@ -0,0 +1,76 @@
+#include "keyboard_and_mouse.h"
+
+const char* Keyboard_And_Mouse_Settings[] =
+{
+	// Keyboard_&_Mouse_Settings
+	"mouse_inverty",
+	"option_duck_method",
+	"option_speed_method",
+	"cl_debounce_zoom",
+	"sensitivity",
+	"zoom_sensitivity_ratio",
+
+	// Movement_Keys
+	"show_loadout_toggle",
+	"+forward",
+	"+back",
+	"+left",
+	"+right",
+	"+sprint",
+	"+duck",
+	"+jump",
+
+	// Weapon_Keys
+	"+use",
+	"+attack",
+	"+attack2",
+	"+reload",
+	Radial_Weapon_Menu,
+	Select_Previous_Weapon,
+	Select_Next_Weapon,
+	Last_Weapon_Used,
+	"drop",
+	"+lookatweapon",
+	Switch_Viewmodel_Left_Right_Hand,
+	"buymenu",
+	"autobuy",
+	Rebuy,
+	"slot1",
+	"slot2",
+	"slot3",
+	"slot4",
+	"slot5",
+	"slot6",
+	"slot7",
+	"slot8",
+	"slot9",
+	"slot10",
+	"slot11",
+	"slot12",
+	"slot13",
+	"+spray_menu",
+
+	// Ul_Keys
+	"+showscores",
+	"+cl_show_team_equipment",
+	Toggle_Radar_Zoom,
+	"callvote",
+	"teammenu",
+	"toggleconsole",
+
+	// Communication_Options
+	"player_ping",
+	Radio_Message,
+	Command_Radio_Message,
+	Standard_Radio_Message,
+	Report_Radio_Message,
+	"messagemode2",
+	"messagemode",
+	"+voicerecord",
+	"clutch_mode_toggle",
+
+	// Chat_Wheel_Keys
+	Chat_Wheel_1,
+	Chat_Wheel_2,
+	Chat_Wheel_3
+};
