8f0e77f9bf84ec71eef206002c69c474a134d8ac
Agus <valenzuela.agustin.tomas@gmail.com>
2025-06-12 22:31:35 -0300

    Bug fix for search (quoted) target string

--- libs/nstd_string/nstd_string.c
+++ libs/nstd_string/nstd_string.c
@@ -4,6 +4,8 @@
 
 long strlength(const char* string)
 {
+	if (string == NULL)
+		return 0;
 	long i;
 	for (i = 0; string[i]; i++);
 	return i;
@@ -11,8 +13,11 @@ long strlength(const char* string)
 
 char* copy_file_to_string(const char* file_name)
 {
+	if (file_name == NULL)
+		return NULL;
+
 	FILE* file = fopen(file_name, "rb");
-	char* string = NULL;
+	char* string;
 	long size;
 
 	if (file == NULL)
@@ -21,11 +26,17 @@ char* copy_file_to_string(const char* file_name)
 	(void)fseek(file, 0, SEEK_END);
 	size = ftell(file);
 	if (size <= 0)
+	{
+		(void)fclose(file);
 		return NULL;
+	}
 
 	string = (char*) calloc(size + 1, 1);
 	if (string == NULL)
+	{
+		(void)fclose(file);
 		return NULL;
+	}
 
 	(void)fseek(file, 0, SEEK_SET);
 	(void)fread(string, 1, size, file);
@@ -40,11 +51,12 @@ long search_for_target_string(const char* target_string, const char* string)
 	if (target_string == NULL || string == NULL)
 		return INVALID_INPUT;
 
-	long i, j;
+	long i, j, start;
 
-	for (i = 0, j = 0; string[i] && target_string[j];)
+	for (i = 0, j = 0, start = 0; string[i] && target_string[j]; i = start + 1)
 	{
 		for (; string[i] && string[i] != target_string[0]; i++);
+		start = i;
 		for (j = 0; string[i] && target_string[j] && string[i] == target_string[j]; i++, j++);
 	}
 
@@ -66,9 +78,10 @@ long search_for_quoted_target_string(const char* target_string, const char* stri
 		for (j = 0, i++; string[i] && target_string[j] && string[i] == target_string[j]; i++, j++);
 	}
 
-	return (target_string[j]) ? STRING_NOT_FOUND : i - j;
+	return (target_string[j] || !string[i]) ? STRING_NOT_FOUND : i - j;
 }
 
+/*
 long search_string_in_file(const char* target_string, const char* file_name)
 {
 	if (target_string == NULL || file_name == NULL || *target_string == '\0')
@@ -121,3 +134,4 @@ long search_string_in_file(const char* target_string, const char* file_name)
 
 	return (is_equal) ? current_position_in_file : STRING_NOT_FOUND;
 }
+*/
--- libs/nstd_string/nstd_string.h
+++ libs/nstd_string/nstd_string.h
@@ -11,6 +11,12 @@ enum
 #define TO_LOWER(c) ( ((c) >= 'A' && (c) <= 'Z') ? (c) | (1<<5) : (c) )
 #define TO_UPPER(c) ( ((c) >= 'a' && (c) <= 'z') ? (c) & ~(1<<5) : (c) )
 
+#define IS_A_DIGIT(c) ( (c) <= '9' && (c) >= '0' )
+#define IS_A_SIGN(c) ( (c) == '-' || (c) == '+' )
+#define IS_A_POINT(c) ( (c) == '.' )
+#define IS_AN_INTEGER_NUMBER(c) ( IS_A_DIGIT(c) || IS_A_SIGN(c) )
+#define IS_A_REAL_NUMBER(c) ( IS_A_DIGIT(c) || IS_A_POINT(c) || IS_A_SIGN(c) )
+
 long strlength(const char* string);
 
 char* copy_file_to_string(const char* file_name);
@@ -19,6 +25,6 @@ long search_for_target_string(const char* target_string, const char* string);
 
 long search_for_quoted_target_string(const char* target_string, const char* string);
 
-long search_string_in_file(const char* target_string, const char* file_name);
+// long search_string_in_file(const char* target_string, const char* file_name);
 
 #endif
